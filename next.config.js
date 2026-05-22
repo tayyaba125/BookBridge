7a29fdaf51b0a82957aa9bcbc01f9217435ef154
tayyaba125 <117144475+tayyaba125@users.noreply.github.com>
2026-05-22 15:04:02 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -3,6 +3,10 @@ const nextConfig = {
   images: {
     domains: ['covers.openlibrary.org'],
   },
+  output: 'standalone',
+  experimental: {
+    serverComponentsExternalPackages: ['@prisma/client', 'bcryptjs'],
+  },
 }
 
 module.exports = nextConfig
