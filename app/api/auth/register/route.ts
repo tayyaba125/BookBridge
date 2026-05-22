6d874d412304f300b5827185a08da5dd7a475ad5
tayyaba125 <117144475+tayyaba125@users.noreply.github.com>
2026-05-22 16:00:05 +0530

    Update route.ts

--- app/api/auth/register/route.ts
+++ app/api/auth/register/route.ts
@@ -1,4 +1,6 @@
 export const dynamic = 'force-dynamic'
+export const runtime = 'nodejs'
+
 import { NextRequest, NextResponse } from 'next/server'
 import bcrypt from 'bcryptjs'
 import { prisma } from '@/lib/prisma'
